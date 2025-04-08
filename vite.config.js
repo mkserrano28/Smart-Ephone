fae1ce2ad433e63e5bea4942b2912ca66cf6bf67
mkserrano28 <mkserrano28@gmail.com>
2025-04-08 16:31:25 +0800

    fix server connection 8th

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  define: {
+    'process.env': process.env,  // Make sure environment variables are available in the app
+  },
 })
