e9107d48096930bf2b62e99251c445100ec70963
mkserrano28 <mkserrano28@gmail.com>
2025-06-05 13:56:32 +0800

    Added Profile edit

--- src/pages/userProfile.jsx
+++ src/pages/userProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-export default function userProfile() {
+export default function UserProfile() {
 
   return (
     <div className="max-w-xl mx-auto mt-30 p-6 bg-white dark:bg-gray-800 rounded shadow text-black dark:text-white">
