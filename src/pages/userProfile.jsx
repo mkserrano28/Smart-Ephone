65023e0b23cf674f8c4f7c1f186d4311e8ad3d1b
mkserrano28 <mkserrano28@gmail.com>
2025-06-05 14:10:55 +0800

    Added Profile edit1

--- src/pages/userProfile.jsx
+++
@@ -1,9 +0,0 @@
-export default function UserProfile() {
-  return (
-    <>
-      <div className="max-w-xl mx-auto mt-10 p-6 bg-white dark:bg-gray-800 rounded shadow text-black dark:text-white">
-        <h1>Username</h1>
-      </div>
-    </>
-  );
-}
